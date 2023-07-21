f8ac2d52c2df26d12c50c29af2abdae25dd32d6e
Chambers <bobhus394@gmail.com>
2023-07-21 10:17:21 +0800

    api更新 移除獲得圖片詳細資訊功能

--- components/card.vue
+++ components/card.vue
@@ -6,22 +6,6 @@
   <img @load="loaded = true" class="aspect-square" :src="props.job.image_url" alt="ai image">
 </figure>
 <div class=" p-3">
-  <div v-if="!loading">
-    <h3 class="font-bold">正面提詞 
-      <button @click="copy(jobDetail.params.prompt)" type="button">
-        <i class='bx bxs-copy-alt bx-sm' v-if="copied"></i>
-        <i class='bx bx-copy-alt bx-sm' v-else ></i>
-      </button>
-    </h3>
-    <p class="text-xs line-clamp-1">{{ jobDetail.params.prompt }}</p>
-    <h3 class="font-bold">負面提詞 
-      <button @click="negCopy(jobDetail.params.prompt)" type="button">
-        <i class='bx bxs-copy-alt bx-sm' v-if="negCopied"></i>
-        <i class='bx bx-copy-alt bx-sm' v-else ></i>
-      </button>
-    </h3>
-    <p class="text-xs line-clamp-1">{{ jobDetail.params.negative_prompt }}</p>
-  </div>
   <div class=" grid grid-cols-3 gap-2">
     <button @click.stop="deleteImage" type="button" class="transition-transform hover:-translate-y-1 col-start-3 rounded-lg p-3 bg-secondary-button">
       <i class='bx bx-trash bx-sm'></i>
@@ -40,20 +24,6 @@ const props = defineProps({
   delete: Function
 })
 
-const {
-  copy: negCopy,
-  copied: negCopied
-} = useClipboard({
-  copiedDuring: 500
-})
-
-const {
-  copy,
-  copied
-} = useClipboard({
-  copiedDuring: 500
-})
-
 const show = ref(false)
 const loaded = ref(false)
 
@@ -63,12 +33,5 @@ const deleteImage = async () => {
   })
   show.value = false
 }
-
-const {
-  getDetail,
-  loading,
-  jobDetail
-} = useImages()
-getDetail(props.job.id)
 
 </script>
